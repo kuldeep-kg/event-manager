4eac9405e0fd2d30cacb129a4e4c87ded875b22e
unknown <kldpgarg1@gmail.com>
2021-05-06 12:58:02 +0530

    Modify the cloud function to publish and process the event message using watermill

--- cloudfunctions/go.mod
+++ cloudfunctions/go.mod
@@ -3,6 +3,8 @@ module github.com/maira-io/event-manager/cloudfunctions
 go 1.16
 
 require (
+	github.com/ThreeDotsLabs/watermill v1.2.0-rc.3
+	github.com/ThreeDotsLabs/watermill-googlecloud v1.0.6
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/maira-io/apiserver v0.0.0-20210430181802-b519a75b055d
 	github.com/prometheus/alertmanager v0.21.0
--- cloudfunctions/httpcloudfunction.go
+++ cloudfunctions/httpcloudfunction.go
@@ -1,8 +1,10 @@
 package cloudfunctions
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"encoding/json"
 
@@ -10,12 +12,47 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-googlecloud/pkg/googlecloud"
+	"github.com/ThreeDotsLabs/watermill/message"
+
 	event_pb "github.com/maira-io/apiserver/generated/event"
 	"github.com/prometheus/alertmanager/template"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func PrometheusWebhook(w http.ResponseWriter, r *http.Request) {
+func PrometheusWebhookWithWatermill(w http.ResponseWriter, r *http.Request) {
+	logger := watermill.NewStdLogger(false, false)
+	subscriber, err := googlecloud.NewSubscriber(
+		googlecloud.SubscriberConfig{
+			// custom function to generate Subscription Name,
+			// there are also predefined TopicSubscriptionName and TopicSubscriptionNameWithSuffix available.
+			GenerateSubscriptionName: func(topic string) string {
+				return "maira-sub_" + topic
+			},
+			ProjectID: "maira-event-manager",
+		},
+		logger,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Subscribe will create the subscription. Only messages that are sent after the subscription is created may be received.
+	messages, err := subscriber.Subscribe(context.Background(), "event.topic")
+	if err != nil {
+		panic(err)
+	}
+
+	go process(messages)
+
+	publisher, err := googlecloud.NewPublisher(googlecloud.PublisherConfig{
+		ProjectID: "maira-event-manager",
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
 	defer r.Body.Close()
 
 	data := template.Data{}
@@ -37,6 +74,9 @@ func PrometheusWebhook(w http.ResponseWriter, r *http.Request) {
 			Annotations: alert.Annotations,
 		}
 		log.Println(event)
+		payload, _ := json.Marshal(event)
+
+		publishMessages(publisher, payload)
 	}
 
 	fmt.Fprint(w, "successfully processed")
@@ -49,4 +89,24 @@ func getHost(externalurl string) string {
 	}
 	host, _, _ := net.SplitHostPort(u.Host)
 	return host
+}
+
+func publishMessages(publisher message.Publisher, payload message.Payload) {
+	msg := message.NewMessage(watermill.NewUUID(), payload)
+
+	if err := publisher.Publish("event.topic", msg); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(time.Second)
+}
+
+func process(messages <-chan *message.Message) {
+	for msg := range messages {
+		log.Printf("received message: %s, payload: %s", msg.UUID, string(msg.Payload))
+
+		// we need to Acknowledge that we received and processed the message,
+		// otherwise, it will be resent over and over again.
+		msg.Ack()
+	}
 }
