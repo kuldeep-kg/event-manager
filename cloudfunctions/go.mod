a1c66d0a2349e26267f49cbd11de89b73d06eaf4
unknown <kldpgarg1@gmail.com>
2021-05-12 21:28:28 +0530

    update test file to convert main function to test. reformat

--- cloudfunctions/go.mod
+++ cloudfunctions/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/prometheus/client_golang v1.10.0 // indirect
 	github.com/prometheus/common v0.23.0 // indirect
 	github.com/shurcooL/vfsgen v0.0.0-20200824052919-0d455de96546 // indirect
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20210503173754-0981d6026fa6 // indirect
 	google.golang.org/protobuf v1.26.0
 )
--- cloudfunctions/test/my_test.go
+++ cloudfunctions/test/my_test.go
@@ -1,12 +1,15 @@
-package main
+package test
 
 import (
 	"context"
+	"log"
+	"testing"
 
 	"github.com/maira-io/event-manager/cloudfunctions"
+	"github.com/stretchr/testify/assert"
 )
 
-func main() {
+func TestWatermill(t *testing.T) {
 	router := cloudfunctions.CreateWatermillRouter()
 
 	subscriber := cloudfunctions.CreateWatermillSubscriber()
@@ -14,16 +17,20 @@ func main() {
 	publisher := cloudfunctions.CreateWatermillPublisher()
 
 	cloudfunctions.SetHandlers(router, subscriber, publisher)
+	log.Println("Created handler")
 
 	// Now that all handlers are registered, we're running the Router.
 	// Run is blocking while the router is running.
 	ctx := context.Background()
 	if err := router.Run(ctx); err != nil {
 		panic(err)
+		t.Fatal(err)
 	}
 
 	msg := "Hello! This is test message."
+	log.Printf("sending message %s\n", msg)
 
 	cloudfunctions.PublishMessages(publisher, []byte(msg))
+	assert.NotNil(t, router)
 
 }
